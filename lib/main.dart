05fa17f5dde1baf575a6be9cfa220c2efd72f6a6
singhkunal2050 <singhkunal2050@gmail.com>
2020-06-18 14:18:34 +0530

    using object text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './Quote.dart';
 
 void main() => runApp(ListApp());
 
@@ -8,7 +9,11 @@ class ListApp extends StatefulWidget {
 }
 
 class _ListAppState extends State<ListApp> {
-  List<String> qoutes = ["Im the best", "You are the best", "Love nature"];
+  List<Quote> qoutes = [
+    Quote(text: "Im the best", author: "Kunal Singh"),
+    Quote(text: "You the best", author: "Karan Singh"),
+    Quote(text: "Save the best", author: "Komal Singh")
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +27,9 @@ class _ListAppState extends State<ListApp> {
         body: Column(
           children: qoutes.map((qoute) {
             return Text(
-              qoute,
+              '${qoute.text} ~ ${qoute.author}',
               style: TextStyle(
-                fontSize: 20,
+                fontSize: 30,
               ),
             );
           }).toList(),
@@ -34,12 +39,9 @@ class _ListAppState extends State<ListApp> {
   }
 }
 
-
-// childrens of a col/row can be given by passing map to lists with a widget return 
-// toList function and maps can be used togerher with row/col etc 
+// childrens of a col/row can be given by passing map to lists with a widget return
+// toList function and maps can be used togerher with row/col etc
 // children : arr.map(elem){
 //     return Text(elem)
 // }).toList()
 // children: qoutes.map((qoute)=> Text(qoute)).toList();
-
-
