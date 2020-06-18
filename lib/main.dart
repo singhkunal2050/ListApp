d19f2fe4df905cc455da9c49fe0add89d414dc12
singhkunal2050 <singhkunal2050@gmail.com>
2020-06-18 13:55:10 +0530

    tolist with map

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ class ListApp extends StatefulWidget {
 }
 
 class _ListAppState extends State<ListApp> {
+  List<String> qoutes = ["Im the best", "You are the best", "Love nature"];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,18 +17,29 @@ class _ListAppState extends State<ListApp> {
         appBar: AppBar(
           title: Text("ListApp"),
           centerTitle: true,
+          backgroundColor: Colors.redAccent,
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Text(
-                "Heyy",
-                style: TextStyle(fontSize: 50),
+        body: Column(
+          children: qoutes.map((qoute) {
+            return Text(
+              qoute,
+              style: TextStyle(
+                fontSize: 20,
               ),
-            ],
-          ),
+            );
+          }).toList(),
         ),
       ),
     );
   }
 }
+
+
+// childrens of a col/row can be given by passing map to lists with a widget return 
+// toList function and maps can be used togerher with row/col etc 
+// children : arr.map(elem){
+//     return Text(elem)
+// }).toList()
+// children: qoutes.map((qoute)=> Text(qoute)).toList();
+
+
